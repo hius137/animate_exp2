7407be6ca0d895920e17a9ce82b8afe650a21863
hieuvm <hieuvm.nws@gmail.com>
2024-04-02 10:41:06 +0700

    Initial Commit

---
+++ animate_exp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animate_exp2
+//
+//  Created by HieuVM3 on 02/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ animate_exp2UITests/animate_exp2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  animate_exp2UITestsLaunchTests.swift
+//  animate_exp2UITests
+//
+//  Created by HieuVM3 on 02/04/2024.
+//
+
+import XCTest
+
+final class animate_exp2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
